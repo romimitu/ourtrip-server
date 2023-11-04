b97f7217d9814c095416b2122fde1956b450f0af
RoMi <romimitu@gmail.com>
2023-11-05 01:24:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-const uri = `mongodb+srv://ourtrip_travel:jUbFonN3JaPzFDFM@cluster0.wellz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wellz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
